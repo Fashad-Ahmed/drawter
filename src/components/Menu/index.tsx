8e4c8eddbc0a0ba8b4d4a507c27eaf7bbb48d25b
Fashad-Ahmed <fashad.ahmed@koderlabs.com>
2024-03-10 00:52:16 +0500

    Feat: UI for Menu is completed

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -10,24 +10,23 @@ import styles from "./index.module.css";
 
 const Menu = () => {
   return (
-    <div>
-      <div>
+    <div className={styles.menuContainer}>
+      <div className={styles.iconWrapper}>
         <FontAwesomeIcon icon={faPencil} className={styles.icon} />
       </div>
-      <div>
+      <div className={styles.iconWrapper}>
         <FontAwesomeIcon icon={faEraser} className={styles.icon} />
       </div>
-      <div>
+      <div className={styles.iconWrapper}>
         <FontAwesomeIcon icon={faRotateLeft} className={styles.icon} />
       </div>
-      <div>
+      <div className={styles.iconWrapper}>
         <FontAwesomeIcon icon={faRotateRight} className={styles.icon} />
       </div>
-      <div>
+      <div className={styles.iconWrapper}>
         <FontAwesomeIcon icon={faFileArrowDown} className={styles.icon} />
       </div>
     </div>
-
   );
 };
 
