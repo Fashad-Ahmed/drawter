d0004c444c767c862e817f3574822c8b899f4c05
Fashad-Ahmed <fashad.siddiqui@dubizzlelabs.com>
2024-12-17 23:59:11 +0500

    🛠️ wip: Board in progress

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,26 +1,37 @@
 import { memo, useEffect, useRef } from "react";
 
 import { useActionMenuItem, useActiveMenuItem } from "@/store/menuStore";
-import { useChangeBrushSize, useChangeColor } from "@/store/toolBoxStore";
+import { useToolboxStore } from "@/store/toolBoxStore";
+import { getBrushTool } from "@/utils/helpers";
+import { BrushTool } from "@/utils/types/toolBox.type";
 
 const Board = () => {
   const canvasRef = useRef(null);
+  const shouldDraw = useRef<boolean>(false);
 
   const activeMenuItem = useActiveMenuItem();
   const actionMenuItem = useActionMenuItem();
-  const color = useChangeColor(activeMenuItem);
-  const brushSize = useChangeBrushSize(activeMenuItem);
 
-  const changeConfig = (color: string, size: number, context: any) => {
-    context.strokeStyle = color;
-    context.lineWidth = size;
-  };
+  const { color, size: brushSize } = useToolboxStore((state) => {
+    const item = state[activeMenuItem] as BrushTool;
+    return item;
+  });
 
   useEffect(() => {
     if (!canvasRef.current) return;
 
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
+
+    context.strokeStyle = color;
+    context.lineWidth = brushSize;
+
+    const changeConfig = (color: string, size: number) => {
+      context.strokeStyle = color;
+      context.lineWidth = size;
+    };
+
+    changeConfig(color, brushSize);
   }, [color, brushSize]);
 
   useEffect(() => {
@@ -28,10 +39,51 @@ const Board = () => {
 
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
-    console.log("🚀 ~ useEffect ~ context:", context);
-
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    const beginPath = (x, y) => {
+      context.beginPath();
+      context.moveTo(x, y);
+    };
+
+    const drawLine = (x, y) => {
+      context.lineTo(x, y);
+      context.stroke();
+    };
+
+    const handleMouseDown = (e) => {
+      shouldDraw.current = true;
+      // beginPath(
+      //   context,
+      //   e.clientX || e?.touches?.[0].clientX,
+      //   e.clientY || e?.touches?.[0].clientY
+      // );
+    };
+
+    const handleMouseMove = (e) => {
+      if (!shouldDraw.current) return;
+
+      // drawLine(
+      //   context,
+      //   e.clientX || e?.touches?.[0].clientX,
+      //   e.clientY || e?.touches?.[0].clientY
+      // );
+    };
+
+    const handleMouseUp = (e) => {
+      shouldDraw.current = false;
+    };
+
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      canvas.addEventListener("mousedown", handleMouseDown);
+      canvas.addEventListener("mousemove", handleMouseMove);
+      canvas.addEventListener("mouseup", handleMouseUp);
+    };
   }, []);
 
   return <canvas ref={canvasRef}></canvas>;
